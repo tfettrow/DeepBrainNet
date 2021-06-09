ecf677fef84f6b1ddfb9a9005e60d30901624c5d
tfettrow <tfettrow@gmail.com>
2021-06-09 15:37:34 -0400

    not needed

--- Script/shell/skull_strip.sh
+++
@@ -1,8 +0,0 @@
-	
-
-cp ${Template_dir}/TPM.nii ${Subject_dir}
-cd ${Subject_dir}
-gunzip -f *nii.gz 
-matlab -nodesktop -nosplash -r "try; segment_t1; catch; end; quit"
-matlab -nodesktop -nosplash -r "try; skull_strip_t1; catch; end; quit"
-rm TPM.nii
